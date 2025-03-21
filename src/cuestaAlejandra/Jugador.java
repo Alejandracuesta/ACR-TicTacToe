90dcd93fd79ac9fbf081d0b829823b17e5c09921
Alejandra Cuesta <alejandra.cuesta@alumnos.uneatlantico.es>
2025-03-21 10:24:05 +0100

    mover ficha

--- src/cuestaAlejandra/Jugador.java
+++ src/cuestaAlejandra/Jugador.java
@@ -13,4 +13,14 @@ public class Jugador {
         } while(!coordenada.esValida() || tablero.estaOcupado(coordenada));
         tablero.ponerFicha(coordenada,color);
     }
+
+    public void moverFicha(Tablero tablero) {
+        Coordenada coordenada = new Coordenada();
+        do{
+            coordenada.pedir();
+        } while(!coordenada.esValida() || tablero.estaVacio(coordenada));
+        tablero.sacarFicha(coordenada);
+        this.ponerFicha(tablero);
+    }
+
 }
